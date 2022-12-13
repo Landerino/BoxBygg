cfb8e8fb1f338d717c41e3072f2c07ba7493c768
Landerino <max.hellenberg@yh.nackademin.se>
2022-12-13 12:02:33 +0100

    Player Name Fix
    Players nickname in session now rotates towards you, making it easier to read names

--- Assets/BoxBygg/Project/Main/Scripts/billboard.cs
+++ Assets/BoxBygg/Project/Main/Scripts/billboard.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class billboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Camera cam;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (cam == null)
+            cam = FindObjectOfType<Camera>();
+        if (cam == null)
+            return;
+
+        transform.LookAt(cam.transform);
+        transform.Rotate(Vector3.up * 180);
     }
 }
