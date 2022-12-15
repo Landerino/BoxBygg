036d96e8264a7ef4d83b4de8c5d57ae1f792ff9d
Landerino <max.hellenberg@yh.nackademin.se>
2022-12-15 14:28:52 +0100

    Matching Hand Skin
    The users hands now match selected avatars color. Still only placeholder for the real avatars but interchangeable whenever

---
+++ Assets/BoxBygg/Project/Max/Scripts/HandSkin.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandSkin : MonoBehaviour
+{
+    public SceneInfo sceneInfo;
+
+    public Color32[] skinColorArray;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.SetColor("_Color", skinColorArray[sceneInfo.playerSkinNo]);
+    }
+    
+    public void setSkinColor(int number)
+    {
+        sceneInfo.playerSkinNo = number;
+        UpdateHandSkin();
+    }
+
+    private void UpdateHandSkin()
+    {
+        rend.material.SetColor("_Color", skinColorArray[sceneInfo.playerSkinNo]);
+    }
+
+}
---
+++ Assets/BoxBygg/Project/Max/Scripts/LoadSkin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSkin : MonoBehaviour
+{
+    public SceneInfo sceneInfo;
+
+    void Start()
+    {
+        sceneInfo.playerSkinNo = 0;
+    }
+
+}
