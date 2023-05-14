93ef654a868c93dfb916e98eab8908a97867a67d
Landerino <max.hellenberg@yh.nackademin.se>
2023-05-14 21:35:23 +0200

    Networked Clipboard
    Adapted the clipboard to be fully synced online across users

--- Assets/BoxBygg/Project/Main/Scripts/ClipboardScript.cs
+++ Assets/BoxBygg/Project/Main/Scripts/ClipboardScript.cs
@@ -1,43 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ClipboardScript : MonoBehaviour
 {
-    private bool IsOn;
-    public GameObject Check;
-    public GameObject Roof;
+    PhotonView PV;
+
+    private bool IsOn1;
+    private bool IsOn2;
+    private bool IsOn3;
+
+    public GameObject WaterPipes;
+    public GameObject ElectricityCables;
+    public GameObject AirVents;
+    public GameObject Check1;
+    public GameObject Check2;
+    public GameObject Check3;
 
     void Start()
     {
-        IsOn = false;
-        Check.SetActive(false);
-        Roof.SetActive(false);
+        PV = GetComponent<PhotonView>();
+        IsOn1 = false;
+        IsOn2 = false;
+        IsOn3 = false;
+        WaterPipes.SetActive(false);
+        ElectricityCables.SetActive(false);
+        AirVents.SetActive(false);
+        Check1.SetActive(false);
     }
 
-    public void Switch()
+    public void Switch(int Check2)
     {
-        if (!IsOn)
+        string Number = Check2.ToString();
+        PV.RPC("ShowHide" + Number, RpcTarget.All);
+    }
+
+    [PunRPC]
+    void ShowHide1()
+    {
+        Debug.Log("in 1");
+        if (!IsOn1)
         {
-            Check.SetActive(true);
-            IsOn = true;
+            IsOn1 = true;
+            WaterPipes.SetActive(true);
+            Check1.SetActive(true);
         }
         else
         {
-            Check.SetActive(false);
-            IsOn = false;
+            IsOn1 = false;
+            WaterPipes.SetActive(false);
+            Check1.SetActive(false);
         }
     }
 
-    public void RemoveRoof()
+    [PunRPC]
+    void ShowHide2()
     {
-        if (IsOn)
+        Debug.Log("in 2");
+        if (!IsOn2)
         {
-            Roof.SetActive(true);
+            IsOn2 = true;
+            ElectricityCables.SetActive(true);
+            Check2.SetActive(true);
         }
         else
         {
-            Roof.SetActive(false);
+            IsOn2 = false;
+            ElectricityCables.SetActive(false);
+            Check2.SetActive(false);
+        }
+    }
+
+    [PunRPC]
+    void ShowHide3()
+    {
+        Debug.Log("in 3");
+        if (!IsOn3)
+        {
+            IsOn3 = true;
+            AirVents.SetActive(true);
+            Check3.SetActive(true);
+        }
+        else
+        {
+            IsOn3 = false;
+            AirVents.SetActive(false);
+            Check3.SetActive(false);
         }
     }
 }
